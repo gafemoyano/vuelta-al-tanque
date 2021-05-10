5fe42757960923351fd386b71c523b5850e592a6
Felipe Moyano <gafemoyano@gmail.com>
2021-05-09 20:24:33 -0500

    Expermient design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: {
     mode: "all",
@@ -7,9 +9,54 @@ module.exports = {
     },
   },
   theme: {
+    fontFamily: {
+      display: [
+        "Abril Fatface",
+        "system-ui",
+        "-apple-system",
+        "BlinkMacSystemFont",
+        '"Segoe UI"',
+        "Roboto",
+        '"Helvetica Neue"',
+        "Arial",
+        '"Noto Sans"',
+        "sans-serif",
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+      body: [
+        "Inter",
+        "system-ui",
+        "-apple-system",
+        "BlinkMacSystemFont",
+        '"Segoe UI"',
+        "Roboto",
+        '"Helvetica Neue"',
+        "Arial",
+        '"Noto Sans"',
+        "sans-serif",
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+    },
     container: {
       center: true,
     },
+    colors: {
+      white: "white",
+      transparent: "transparent",
+      current: "currentColor",
+      blue: colors.blue,
+      gray: colors.gray,
+      pink: colors.rose,
+      primary: "#5D62FB",
+      secondary: colors.rose[300],
+    },
+
     extend: {
       colors: {},
     },
