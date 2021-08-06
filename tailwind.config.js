b0eca10896832d2d441b3cb86203565c1f16f7e4
Felipe Moyano <gafemoyano@hotmail.com>
2021-08-06 17:57:20 -0500

    add a rad proposal

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ module.exports = {
   theme: {
     fontFamily: {
       display: [
-        '"Noto Sans"',
+        '"Lilita One"',
         "ui-serif",
         "Georgia",
         "Cambria",
@@ -44,11 +44,16 @@ module.exports = {
       white: "white",
       transparent: "transparent",
       current: "currentColor",
-      blue: colors.blue,
-      gray: colors.gray,
+      blue: colors.lightBlue,
       pink: colors.rose,
-      primary: colors.lightBlue[900],
-      secondary: colors.amber[200],
+      text: '#04313c',
+      background: '#c4eaef',
+      primary: colors.lightBlue[600],
+      secondary: '#5CE1E5',
+      // secondary: '#00c2d7',
+      muted: colors.lightBlue[200],
+      accent: colors.pink[500],
+      gray: colors.coolGray[800],
     },
 
     extend: {
@@ -63,3 +68,17 @@ module.exports = {
   },
   plugins: [require("@tailwindcss/typography")],
 };
+const cyan = {
+  cyan1: '#fafdfe',
+  cyan2: '#f2fcfd',
+  cyan3: '#e7f9fb',
+  cyan4: '#d8f3f6',
+  cyan5: '#c4eaef',
+  cyan6: '#aadee6',
+  cyan7: '#84cdda',
+  cyan8: '#3db9cf',
+  cyan9: '#05a2c2',
+  cyan10: '#0894b3',
+  cyan11: '#0c7792',
+  cyan12: '#04313c',
+}
